8009d24388885b18d776a7df017009015f25e669
daniellasheva <daniellasheva@gmail.com>
2016-07-26 17:14:08 -0400

    first commit

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,9 @@
 class Dog
     
-    fido=Dog.new
-    
-    snoopy= Dog.new
-    
-    lassie=Dog.new
-    
 end
+
+fido=Dog.new
+
+snoopy= Dog.new
+
+lassie=Dog.new
--- lib/person.rb
+++ lib/person.rb
@@ -1,9 +1,8 @@
-class Dog
-    
-    fido=Dog.new
-    
-    snoopy= Dog.new
-    
-    lassie=Dog.new
+class Person
     
 end
+
+adele_goldberg= Person.new
+
+alan_kay= Person.new
+
